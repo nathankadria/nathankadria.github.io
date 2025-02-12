fcf3b023e94c1d19c4d746a12824aa794c84665a
nathankadria <nathankadria@gmail.com>
2025-02-12 02:37:38 +0000

    Deploying to gh-pages from @ nathankadria/nathankadria.github.io@ab9fd0581e1839434eb68da6ebe6ccd3f411d007 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "UNDER CONSTRUCTION",
+          description: "a non-exhaustive ever-growing list",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
